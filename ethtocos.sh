ec65cf796685f5e57be451c26d95412d543de669
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-12-21 17:09:16 +0700

    Update ethtocos.sh

--- ethtocos.sh
+++ ethtocos.sh
@@ -6,4 +6,5 @@ ETH_RPC="https://goerli-light.nodes.guru"
 for(( i=1; i <=55; i++ ))
 do
   peggo bridge send-to-cosmos 0xe54fbaecc50731afe54924c40dfd1274f718fe02 umee1sg2vam988aqenkrkdk3er0huuzyupejz8a6j20 10 --eth-pk=$ETH_PK_CLEAN --eth-rpc=$ETH_RPC
+  sleep 60
 done
