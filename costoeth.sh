01321d6093137711ab2d73a6b955e0d88dfc205d
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-12-21 17:07:31 +0700

    Update costoeth.sh

--- costoeth.sh
+++ costoeth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for(( i=1; i <=2; i++ ))
+for(( i=1; i <=55; i++ ))
 do
 	txhash=$(echo 1234jqka12 | umeed tx peggy send-to-eth 0xBaba0eA6D8B8D9CD19f6F1AeD2cDB76888D273D4 10uumee 1uumee --from BotDotNet --chain-id umee-alpha-mainnet-2 -y)
 	sleep 5
@@ -8,7 +8,7 @@ do
 	txhash2=$(cut -d',' -f 1 <<<"$txhash1")
 	txhash3=$(cut -d'"' -f 2 <<<"$txhash2")
 	echo $txhash3 >> /root/costoeth_tx.txt
-	sleep 10
+	sleep 60
 done
 
 printf "%b\n\n\n" "-------"
@@ -24,5 +24,5 @@ do
 	printf '%s\n' "" >> /root/costoeth_raw.txt
 	echo $(umeed q tx $p | grep raw_log) >> /root/costoeth_raw.txt
 	printf '%s\n' "---" >> /root/costoeth_raw.txt
-	sleep 5
+	sleep 10
 done </root/costoeth_tx.txt
